544d3a54f1368aaa751e9470b261e089e89a2576
indemio <indemio!@gmail.com>
2020-06-01 23:00:04 +0800

    upload

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from  flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from datetime import datetime
 
 
 @app.route('/')
@@ -53,8 +54,6 @@ def logout():
     return redirect(url_for('index'))
 
 
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -68,3 +67,37 @@ def register():
         flash('Регистрация прошла успешно')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user=User.query.filter_by(username=username).first_or_404()
+    posts= [
+        {'author': user, 'body': 'Test 1'},
+        {'author': user, 'body': 'Test 2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen=datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username=form.username.data
+        current_user.about_me=form.about_me.data
+        db.session.commit()
+        flash('Изменения успешно сохранены')
+        return redirect(url_for('edit_profile'))
+    elif request.method=="GET":
+        form.username.data=current_user.username
+        form.about_me.data=current_user.about_me
+    return render_template('edit_profile.html', title='Редактировать профиль', form=form)
